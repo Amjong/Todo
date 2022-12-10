4cbc57362b5fcc6cb1f51269baffc659dd9e7d3f
Jongbum <amjong2@gmail.com>
2022-12-10 19:03:30 +0900

    [Todo] Initilaize input value when click input bar & click add button

--- src/component/TodoForm.jsx
+++ src/component/TodoForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 function TodoForm(props) {
   const [todoText, setTodoText] = useState('Add Todo');
@@ -6,12 +7,23 @@ function TodoForm(props) {
     e.preventDefault();
     setTodoText(e.target.value);
   };
+  const handleFirstClick = (e) => {
+    if (e.target.value === 'Add Todo') {
+      setTodoText('');
+    }
+  };
   return (
     <div>
-      <input type='text' value={todoText} onChange={handleChange}></input>
+      <input
+        type='text'
+        value={todoText}
+        onClick={handleFirstClick}
+        onChange={handleChange}
+      ></input>
       <button
         onClick={() => {
           props.addItems(todoText);
+          setTodoText('');
         }}
       >
         Add
